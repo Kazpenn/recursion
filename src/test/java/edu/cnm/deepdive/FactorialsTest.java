bef14fe70e7a554a4aa286740b09d7d0584edc72
Kazpenn <kazpenn2020@gmail.com>
2021-03-09 10:53:17 -0700

    update .gitignore.

--- src/test/java/edu/cnm/deepdive/FactorialsTest.java
+++ src/test/java/edu/cnm/deepdive/FactorialsTest.java
@@ -17,10 +17,10 @@ class FactorialsTest {
   }
 
   @DisplayName("computeRecursive(exceptions)")
-  @ParameterizedTest(name = "[{index}] Asserting computeRecursive({0}) throws IllegalArgumentException.")
+  @ParameterizedTest(name = "[{index}] Asserting computeRecursive({0}) throws {1}.")
   @CsvFileSource(resources = "factorials-test-exception.csv", numLinesToSkip = 1)
-  void computeRecursive_exception(int n) {
-    assertThrows(IllegalArgumentException.class, () -> Factorials.computeRecursive(n));
+  void computeRecursive_exception(int n, Class<? extends Throwable> expectedException) {
+    assertThrows(expectedException, () -> Factorials.computeRecursive(n));
   }
 
 }
