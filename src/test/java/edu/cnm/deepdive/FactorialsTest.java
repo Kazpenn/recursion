95053fa6bfc9be62267d0ddf5ed8a28de9d82853
Kazpenn <kazpenn2020@gmail.com>
2021-03-09 10:08:10 -0700

    Method source returning Arguments[].

--- src/test/java/edu/cnm/deepdive/FactorialsTest.java
+++ src/test/java/edu/cnm/deepdive/FactorialsTest.java
@@ -7,17 +7,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class FactorialsTest {
-
-  static final long[][] testCases = {
-      {0, 1},
-      {1, 1},
-      {5, 120},
-      {10, 3628800},
-      {13, 6227020800L}
-  };
 
   @DisplayName("computeRecursive(non-exceptional)")
   @ParameterizedTest(name = "[{index}] Asserting computeRecursive({0}) == {1}.")
@@ -32,13 +25,13 @@ class FactorialsTest {
     assertThrows(IllegalArgumentException.class, () -> Factorials.computeRecursive(-1));
   }
 
-  static Object[][] computeRecursive_nonException() {
-    return new Object[][] {
-        {0, 1L},
-        {1, 1L},
-        {5, 120L},
-        {10, 3_628_800L},
-        {13, 6_227_020_800L}
+  static Arguments[] computeRecursive_nonException() {
+    return new Arguments[] {
+        Arguments.of(0, 1L),
+        Arguments.of(1, 1L),
+        Arguments.of(5, 120L),
+        Arguments.of(10, 3_628_800L),
+        Arguments.of(13, 6_227_020_800L)
     };
   }
 }
